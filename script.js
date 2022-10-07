f5d8e4c7b45a8f56c08fbdf93cf92442c916a50c
we1der <zakhar.syrovatskii2021@gmail.com>
2022-10-07 22:28:32 +0300

    Added some UI

--- script.js
+++ script.js
@@ -93,6 +93,8 @@ window.addEventListener('load',function(){
             this.x=this.game.width;
             this.speedX=Math.random()* -1.5-0.5;
             this.markedForDeletion=false;
+            this.lives=5;
+            this.score=this.lives;
         }
         update() {
             this.x+=this.speedX;
@@ -101,6 +103,9 @@ window.addEventListener('load',function(){
         draw(context){
             context.fillStyle="red";
             context.fillRect(this.x, this.y, this.width, this.height);
+            context.fillStyle="black";
+            context.font="20px Helvetica";
+            context.fillText(this.lives, this.x, this.y);
         }
     }
 
@@ -125,15 +130,23 @@ window.addEventListener('load',function(){
             this.game=game;
             this.fontSize=25;
             this.fontFamily='Helvetica';
-            this.color='yellow';
+            this.color='white';
         }
 
         draw(context){
-            // ammo
+            context.save();
             context.fillStyle = this.color;
+            context.shadowOffsetX=2;
+            context.shadowOffsetY=2;
+            context.shadowColor="black";
+            context.font=this.fontSize+ "px "+ this.fontFamily;
+            // score
+            context.fillText("Score: " + this.game.score, 20, 40);
+            // ammo
             for (let i=0; i<this.game.ammo; i++){
                 context.fillRect(20+5*i,50,3,20);
             }
+            context.restore();
         }
     }
 
@@ -153,6 +166,8 @@ window.addEventListener('load',function(){
             this.ammoTimer=0;
             this.ammoInterval=500;
             this.gameOver=false;
+            this.score=0;
+            this.winningScore=10;
         }
 
         update(deltaTime){
@@ -163,10 +178,22 @@ window.addEventListener('load',function(){
             } else {
                 this.ammoTimer+=deltaTime;
             }
-            this.enemies.forEach(enemy=>{enemy.update();
+            this.enemies.forEach(enemy=>{
+                enemy.update();
                 if (this.checkCollision(this.player, enemy)){
                     enemy.markedForDeletion=true;
                 }
+                this.player.projectile.forEach(projectile=>{
+                    if (this.checkCollision(projectile,enemy)){
+                        enemy.lives--;
+                        projectile.markedForDeletion=true;          
+                        if (enemy.lives <=0){
+                            enemy.markedForDeletion=true;
+                            this.score+=enemy.score;
+                            if (this.score>this.winningScore) this.gameOver=true;
+                        }
+                    }
+                })
             });
             this.enemies=this.enemies.filter(enemy=>!enemy.markedForDeletion);
             if (this.enemyTimer>this.enemyInterval && !this.gameOver){
